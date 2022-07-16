7dd714697a3da5c27069e26edd8326ad11d3b33c
Victor Inojosa <vijoin@gmail.com>
2022-07-16 17:04:51 -0300

    Handle input errors with message and retry

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,24 @@ use std::io;
 fn main() {
     println!("### Welcome to Guess the number game ###");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
 
     loop {
-
         println!("Please, input your guess");
-        
         let mut guess = String::new();
-        
         io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-        
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
-        
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!(
+                    r#"Error: "{}" is Not a Number, please input a number!"#,
+                    guess.trim()
+                );
+                continue;
+            }
+        };
         println!("You guessed: {guess}");
-        
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
